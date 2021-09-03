673f873d2b2b8b1a110d7ab5f147e9c9f9f34e18
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-09-02 23:58:41 -0400

    fixed the percent function

--- script.js
+++ script.js
@@ -15,17 +15,17 @@ const divide = function(num1, num2) {
 };
 
 const percent = function(num) {
-    return num % 100;
+    return num / 100;
 }
 
 //Calls a function based on the chosen operator
-function operate(operator, num1, num2){
-    if(operator === 'add') 
-        return add(num1, num2);
-    else if(operator === 'subtract')
-        return subtract(num1, num2); 
-    else if(operator === 'multiply')
-        return multiply(num1, num2);
-    else if(operator === 'divide')
-        return divide(num1, num2);
-}
+// function operate(operator, num1, num2){
+//     if(operator === 'add') 
+//         return add(num1, num2);
+//     else if(operator === 'subtract')
+//         return subtract(num1, num2); 
+//     else if(operator === 'multiply')
+//         return multiply(num1, num2);
+//     else if(operator === 'divide')
+//         return divide(num1, num2);
+// }s
