4013e8b1bb5cdedfb97179369934c3b7eca259f3
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-09-03 00:41:38 -0400

    created negate function

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const divide = function(num1, num2) {
 	return num1 / num2;
 };
 
+const negate = function(num){
+    return num * -1;
+}
+
 const percent = function(num) {
     return num / 100;
 }
